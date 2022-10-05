4411182a5c2e873e5618a97552e951a59fa097ef
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-05 00:09:25 -0600

    agregue el efecto de desplazamiento y actualizacion del localstorage

--- src/utils/crearUsuario.js
+++ src/utils/crearUsuario.js
@@ -1,3 +1,4 @@
+import RenderUser from "../components/RenderUser/index.js";
 import LocalStorage from "./localStorage.js";
 import randomNumber from "./randomNumber.js";
 
@@ -5,23 +6,40 @@ function crearUsuario(idGen) {
     const data = LocalStorage().get("Data");
     const entrada = randomNumber(data.entrada[0], data.entrada[1]);
     const salida = randomNumber(data.salida[0], data.salida[1]); 
-    
-    
+    const TimeOut = data.tiempoSalida + salida;
+
     
     setTimeout(() => {
         if (data.cola.length < 10) {
+            const data2 = LocalStorage().get("Data");
             let id = idGen.next().value;
-            data.cola.push(id);
-            console.log(data.cola);
-            LocalStorage().set('Data', data)
+            data2.cola.push(id);
+            data2.tiempoSalida = TimeOut - 2;
+            LocalStorage().set('Data', data2)
+            RenderUser(id, data2.cola.findIndex((e) => e == id))
         }
         crearUsuario(idGen);            
     }, entrada * 1000);
     
+
     setTimeout(() => {
-        data.cola.splice(0,1);
-        LocalStorage().set('Data', data)
-    }, salida * 1000);
+        const data2 = LocalStorage().get("Data");
+        
+        if (data2.cola[0]) {
+            const user = document.getElementById(data2.cola[0])
+            user.remove();
+        }
+        
+        data2.cola.splice(0,1);
+        for (let i = 0; i < data2.cola.length; i++) {
+            const usuario = document.getElementsByClassName("user")[i];
+            const tamano = usuario.style.top.length;
+            let num = usuario.style.top;
+            num = Number(num.split("").splice(0 ,tamano-2).join("")) - 70;
+            usuario.style.top = num + "px";
+        }
+        LocalStorage().set('Data', data2)
+    }, TimeOut * 1000);
 }
 
 export default crearUsuario;
