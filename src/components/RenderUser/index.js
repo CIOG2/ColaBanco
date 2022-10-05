106d359c4b74122810e30ccafd771770692d88d7
Carlos Ivan <carlos.ivan10043@gmail.com>
2022-10-05 00:06:46 -0600

    Renderizado de usuario

--- src/components/RenderUser/index.js
+++ src/components/RenderUser/index.js
@@ -1,11 +1,23 @@
-import Styles from "./style.css" assert { type: 'css' };
+import Styles from "./styles.css" assert { type: 'css' };
 
 const GlobalStyles = [ Styles ];
 document.adoptedStyleSheets = [ ...GlobalStyles , ...document.adoptedStyleSheets ];
 
 
-const RenderUser = () =>{
+const RenderUser = (id, pos) =>{
+    const mapa = document.getElementById("mapa");
 
+    const User = document.createElement("div");
+    User.classList.add("user");
+    User.id = id;
+    User.textContent = "#" + id;
+    setTimeout(() => {
+        const calculo = 90 + (pos* 72)
+        User.style.top = calculo + "px";
+    }, 1);
+
+
+    mapa.append(User)
 }
 
 export default RenderUser;
